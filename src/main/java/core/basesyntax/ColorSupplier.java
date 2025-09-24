f22f499ef57161dbc8448da67a44ee03e873738c
Jakub Cybulski <jakub_cybulski@o2.pl>
2025-09-24 11:09:40 +0200

    fixed minor spacing errors

--- src/main/java/core/basesyntax/ColorSupplier.java
+++ src/main/java/core/basesyntax/ColorSupplier.java
@@ -4,6 +4,7 @@ import java.util.Random;
 
 public class ColorSupplier {
     private final Random rg = new Random();
+
     public String getRandomColor() {
         return (Colors.values()[rg.nextInt(Colors.values().length)]).toString();
     }
